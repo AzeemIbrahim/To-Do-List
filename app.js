1dda549e6ed28686130739ca8a1165a776144f11
azeem <azeemibrahim16@gmail.com>
2023-04-12 23:54:29 +0530

    complete app.js

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 let items = [];
-app.set('view engine', 'ejs');
-
+app.set("view engine", "ejs");
 app.get("/", function (req, res) {
-    let today = new Date();
-    let options = {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-    };
-    let day = (today.toLocaleDateString("en-US", options));
-    res.render("list", { kindofDay: day, newListItems: items })
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+  };
+  let day = today.toLocaleDateString("en-US", options);
+  res.render("list", { kindofDay: day, newListItems: items });
 
-    app.post("/", function (req, res) {
-        let item = req.body.newItem
-        items.push(item);
-        res.redirect("/");
-    })
+  app.post("/", function (req, res) {
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+  });
 });
 app.listen(3000, function () {
-    console.log("Server is live now !");
-
+  console.log("Server is live now !");
 });
