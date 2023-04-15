9dc5b90dc1228e795c7f608b1a728638121cc73a
azeem <azeemibrahim16@gmail.com>
2023-04-15 15:00:51 +0530

    add work-list

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 let items = [];
+let workItems = [];
 app.set("view engine", "ejs");
 app.get("/", function (req, res) {
   let today = new Date();
@@ -13,14 +14,23 @@ app.get("/", function (req, res) {
     month: "long",
   };
   let day = today.toLocaleDateString("en-US", options);
-  res.render("list", { kindofDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 
   app.post("/", function (req, res) {
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    if (req.body.list === "work") {
+      workItems.push(item);
+      res.redirect("/work");
+    } else {
+      items.push(item);
+      res.redirect("/");
+    }
   });
 });
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "work List", newListItems: workItems });
+});
+
 app.listen(3000, function () {
   console.log("Server is live now !");
 });
