001e34fcd0fd928598fc4173397cf807e095a6e7
azeem <azeemibrahim16@gmail.com>
2023-04-15 16:03:14 +0530

    export date module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/views/date.js");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -7,25 +8,18 @@ let items = [];
 let workItems = [];
 app.set("view engine", "ejs");
 app.get("/", function (req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-  };
-  let day = today.toLocaleDateString("en-US", options);
+  let day = date.getDay();
   res.render("list", { listTitle: day, newListItems: items });
-
-  app.post("/", function (req, res) {
-    let item = req.body.newItem;
-    if (req.body.list === "work") {
-      workItems.push(item);
-      res.redirect("/work");
-    } else {
-      items.push(item);
-      res.redirect("/");
-    }
-  });
+});
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  if (req.body.list === "work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 app.get("/work", function (req, res) {
   res.render("list", { listTitle: "work List", newListItems: workItems });
